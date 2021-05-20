e0f4e5acbe1c562eef2c3e60618cb3f40617de28
Greg Perrott <greg@perrott.net.nz>
2021-05-20 15:02:14 +1200

    Added Chinese translation

---
+++ zhTW.lua
@@ -0,0 +1,12 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("GreatVaultStatus", "zhTW")
+
+if not L then
+	return
+end
+
+L["Great Vault Status"] = "寶庫狀態"
+L["Character"] = "角色"
+L["Raids"] = "團隊"
+L["Mythic Dungeons"] = "傳奇地城"
+L["PvP"] = "PvP"
+L["iLevel"] = "裝等"
